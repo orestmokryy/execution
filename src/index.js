12a4422b66bfad5d28f46f9aba0bf9a222fd7a74
BrigYeet <orest.mokryy@gmail.com>
2020-04-16 09:05:48 +0300

    Adding old version

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+
+import App from './App/index';
+import * as serviceWorker from './serviceWorker';
+import reducer from './store/reducer';
+import config from './config';
+
+const store = createStore(reducer);
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter basename={config.basename}>
+            {/* basename="/IOS Smart-Meter" */}
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
+
+
+serviceWorker.unregister();
