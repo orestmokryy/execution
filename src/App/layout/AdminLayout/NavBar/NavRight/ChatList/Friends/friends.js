1b3aa8aa9645d4649883bdb0613de2e468b18a53
BrigYeet <orest.mokryy@gmail.com>
2020-04-16 09:04:02 +0300

    Adding actual version

---
+++ src/App/layout/AdminLayout/NavBar/NavRight/ChatList/Friends/friends.js
@@ -0,0 +1,192 @@
+export default [
+        {
+            id: 1,
+            photo: 'avatar-1.jpg',
+            name: 'Наталя Лелека',
+            new: 3,
+            status: 1,
+            time: 'typing'
+        },
+        {
+            id: 2,
+            photo: 'avatar-2.jpg',
+            name: 'Руслан Васильків',
+            new: 1,
+            status: 1,
+            time: 'online'
+        },
+        {
+            id: 3,
+            photo: 'avatar-3.jpg',
+            name: 'Людмила',
+            status: 1,
+            time: 'online'
+        },
+        {
+            id: 4,
+            photo: 'avatar-1.jpg',
+            name: 'Степан',
+            status: 0,
+            new: 1,
+            time: '10 min ago'
+        },
+        {
+            id: 5,
+            photo: 'avatar-4.jpg',
+            name: 'Suzen',
+            status: 0,
+            time: '15 min ago'
+        },
+        {
+            id: 6,
+            photo: 'avatar-1.jpg',
+            name: 'Галина',
+            new: 3,
+            status: 1,
+            time: 'typing'
+        },
+        {
+            id: 7,
+            photo: 'avatar-2.jpg',
+            name: 'Роман Р.',
+            new: 1,
+            status: 1,
+            time: 'online'
+        },
+        {
+            id: 8,
+            photo: 'avatar-3.jpg',
+            name: 'Людмила',
+            status: 1,
+            time: 'online'
+        },
+        {
+            id: 9,
+            photo: 'avatar-1.jpg',
+            name: 'Степан Р.',
+            status: 0,
+            new: 1,
+            time: '10 min ago'
+        },
+        {
+            id: 10,
+            photo: 'avatar-4.jpg',
+            name: 'Саша',
+            status: 0,
+            time: '15 min ago'
+        },
+        {
+            id: 11,
+            photo: 'avatar-1.jpg',
+            name: 'Роберт Р.',
+            new: 3,
+            status: 1,
+            time: 'typing'
+        },
+        {
+            id: 12,
+            photo: 'avatar-2.jpg',
+            name: 'Роман',
+            new: 1,
+            status: 1,
+            time: 'online'
+        },
+        {
+            id: 13,
+            photo: 'avatar-3.jpg',
+            name: 'Людмила',
+            status: 1,
+            time: 'online'
+        },
+        {
+            id: 14,
+            photo: 'avatar-1.jpg',
+            name: 'Микола',
+            status: 0,
+            new: 1,
+            time: '10 min ago'
+        },
+        {
+            id: 15,
+            photo: 'avatar-4.jpg',
+            name: 'Руслан Васильків',
+            status: 0,
+            time: '15 min ago'
+        },
+        {
+            id: 16,
+            photo: 'avatar-1.jpg',
+            name: 'Руслан Васильків',
+            new: 3,
+            status: 1,
+            time: 'typing'
+        },
+        {
+            id: 17,
+            photo: 'avatar-2.jpg',
+            name: 'Руслан Васильків',
+            new: 1,
+            status: 1,
+            time: 'online'
+        },
+        {
+            id: 18,
+            photo: 'avatar-3.jpg',
+            name: 'Анна',
+            status: 1,
+            time: 'online'
+        },
+        {
+            id: 19,
+            photo: 'avatar-1.jpg',
+            name: 'Анна',
+            status: 0,
+            new: 1,
+            time: '10 min ago'
+        },
+        {
+            id: 20,
+            photo: 'avatar-4.jpg',
+            name: 'Людмила',
+            status: 0,
+            time: '15 min ago'
+        },
+        {
+            id: 21,
+            photo: 'avatar-1.jpg',
+            name: 'Степан Степанів',
+            new: 3,
+            status: 1,
+            time: 'typing'
+        },
+        {
+            id: 22,
+            photo: 'avatar-2.jpg',
+            name: 'Роман',
+            new: 1,
+            status: 1,
+            time: 'online'
+        },
+        {
+            id: 23,
+            photo: 'avatar-3.jpg',
+            name: 'Василь',
+            status: 1,
+            time: 'online'
+        },
+        {
+            id: 24,
+            photo: 'avatar-1.jpg',
+            name: 'Людмила',
+            status: 0,
+            new: 1,
+            time: '10 min ago'
+        },
+        {
+            id: 25,
+            photo: 'avatar-4.jpg',
+            name: 'Людмила',
+            status: 0,
+            time: '15 min ago'
+        }
+];
